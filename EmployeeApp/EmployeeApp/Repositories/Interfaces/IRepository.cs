3b22cfa2da059c393979dc4faaba4ad334b71fc4
ASIBHOMEPC\NAHID <asibcseruet13@gmail.com>
2023-01-15 00:18:03 +0600

    implement employee repository

---
+++ EmployeeApp/EmployeeApp/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : DomainObject
+    {
+        T Get(int id);
+    }
+}
